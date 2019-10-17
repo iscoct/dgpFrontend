c3cf1fd7e9ddd531ccc4ec5db0909cc731c7692d
iscoct <frandkv27@gmail.com>
2019-10-17 13:30:57 +0200

    Included some login examples

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,8 @@
 import React, { PureComponent } from 'react';
-import { Button } from '@material-ui/core';
+import { Grid, TextField, Button } from '@material-ui/core';
+import { Person, Lock, Send } from '@material-ui/icons';
+import { Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 type State = {
     toggle: Boolean;
@@ -8,39 +11,63 @@ type State = {
 type Prop = {};
 
 export default class App extends PureComponent<Prop, State> {
-    constructor(props: Prop) {
-        super(props);
-
-        this.state = {
-            toggle: false
-        };
-
-        this.toggle = this.toggle.bind(this);
-    }
-
     render(): JSX.Element {
-        const { toggle } = this.state;
-        let toRender;
-
-        if (toggle) {
-            toRender = <Button onClick={this.toggle} variant="contained" color="primary">
-                Hello World
-            </Button>
-        } else {
-            toRender = <Button onClick={this.toggle} variant="contained" color="secondary">
-                Another Hello World
-            </Button>
-        }
-        return (
+        return(
             <React.Fragment>
-                {toRender}     
+                <Grid container spacing={1} alignItems="flex-end">
+                    <Grid spacing={1} alignItems="flex-end" container item>
+                        <Grid item>
+                            <Person />
+                        </Grid>
+                        <Grid item>
+                            <TextField label="Usuario" />
+                        </Grid>    
+                    </Grid>
+                    <Grid spacing={1} alignItems="flex-end" container item>
+                        <Grid item>
+                            <Lock />
+                        </Grid>
+                        <Grid item>
+                            <TextField label="Password" />
+                        </Grid>
+                    </Grid>
+                    <Grid container item>
+                        <Button
+                            size="small"
+                            variant="contained"
+                            color="primary"
+                            startIcon={<Send />}
+                        >
+                            Enviar
+                        </Button>
+                    </Grid>
+                </Grid>
+
+                <hr/>
+
+                <Grid container>
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="contained" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Grid>
             </React.Fragment>
         );
-    }
-
-    toggle(event: any): void {
-        this.setState({
-            toggle: ! this.state.toggle
-        });
     }
 }
